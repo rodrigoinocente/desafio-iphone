ff838e2c8c5533483c10b286db4f8d13f288f936
Rodrigo <ridrigocorreia@gmail.com>
2023-09-12 10:57:55 -0300

    first commit

---
+++ src/Iphone.java
@@ -0,0 +1,43 @@
+public class Iphone implements ReprodutorMusical, NavegadorInternet, AparelhoTelefonico {
+    
+    public void tocar(){
+        System.out.println("Tocando musica");
+    }
+
+    public void pausar(){
+        System.out.println("Pausando Musica");
+    }
+
+    public void selecionarMusica(){
+        System.out.println("Selecionando Musica");
+    }
+
+    public void exibirPagina(){
+        System.out.println("Exibindo Pagina");
+    }
+   
+    public void adicionarNovaAba(){
+        System.out.println("Adicionando nova Aba");
+    }
+    
+    public void atualizarPagina(){
+        System.out.println("Atualizando Pagina");
+    }
+
+    public void ligar (){
+        System.out.println("Ligando");
+    }
+
+    public void atender(){
+        System.out.println("Atendendo");
+    }
+
+    public void iniciarCorreioVoz(){
+        System.out.println("Iniciando Correio de Voz");
+    }
+
+
+
+    
+
+}
---
+++ src/Start.java
@@ -0,0 +1,16 @@
+public class Start {
+    public static void main(String[] args) {
+        Iphone iphone = new Iphone();
+
+        iphone.tocar();
+        iphone.pausar();
+        iphone.selecionarMusica();
+        iphone.exibirPagina();
+        iphone.adicionarNovaAba();
+        iphone.atualizarPagina();
+        iphone.ligar();
+        iphone.atender();
+        iphone.iniciarCorreioVoz();
+    }
+    
+}
